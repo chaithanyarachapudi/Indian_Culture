4bd8c5612283464e83d85f7955e941289f4a6a79
chaithanya247 <2200031300@kluniversity.in>
2024-12-08 04:36:24 +0530

    Commit message for submodule

--- java/com/example/demo/repository/UserRepository.java
+++ java/com/example/demo/repository/UserRepository.java
@@ -1,15 +1,13 @@
-package com.cultureheritage.repository;
-
+package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.cultureheritage.model.UserLogin;
-
+import com.example.demo.model.User;
 @Repository
-public interface UserRepository  extends JpaRepository<UserLogin, String>//String - PrimaryKey Datatype
+public interface UserRepository  extends JpaRepository<User, String>//String - PrimaryKey Datatype
 {
 	@Query("select count(u) from User u where u.username=:uname and u.password=:pwd")
 	public String validate(@Param("uname") String uname, @Param("pwd") String pwd);
